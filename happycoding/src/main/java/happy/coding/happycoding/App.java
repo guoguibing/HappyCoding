a97a0a1374a86225a30abc22f1593c6160a4e3fc
Felix <Felix@Felix-PC>
2014-01-10 11:57:18 +0800

    init

---
+++ happycoding/src/main/java/happy/coding/happycoding/App.java
@@ -0,0 +1,13 @@
+package happy.coding.happycoding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

6a5a066493261377ecf3b07f38687f47d522c28d
Felix <Felix@Felix-PC>
2014-02-14 21:40:50 +0800

    Gamma random added

--- happycoding/src/main/java/happy/coding/math/Randoms.java
+++ happycoding/src/main/java/happy/coding/math/Randoms.java
@@ -11,11 +11,10 @@ import java.util.Set;
 
 import org.junit.Test;
 
-public class Randoms
-{
-	private static Random		_random		= new Random(System.currentTimeMillis());
+public class Randoms {
+	private static Random r = new Random(System.currentTimeMillis());
 
-	private static List<Object>	_tempList	= new ArrayList<>();
+	private static List<Object> _tempList = new ArrayList<>();
 
 	/**
 	 * Random generate an integer in [0, range)
@@ -23,14 +22,12 @@ public class Randoms
 	 * @param range
 	 * @return
 	 */
-	public static int uniform(int range)
-	{
+	public static int uniform(int range) {
 		return uniform(0, range);
 	}
 
-	public static void seed(long seed)
-	{
-		_random = new Random(seed);
+	public static void seed(long seed) {
+		r = new Random(seed);
 	}
 
 	/**
@@ -40,16 +37,14 @@ public class Randoms
 	 * @param max
 	 * @return
 	 */
-	public static int uniform(int min, int max)
-	{
-		return min + _random.nextInt(max - min);
+	public static int uniform(int min, int max) {
+		return min + r.nextInt(max - min);
 	}
 
 	/**
 	 * return real number uniformly in [0, 1)
 	 */
-	public static double random()
-	{
+	public static double random() {
 		return uniform();
 	}
 
@@ -59,8 +54,7 @@ public class Randoms
 	 * @param size
 	 *            the size of random array
 	 */
-	public static double[] doubles(int size)
-	{
+	public static double[] doubles(int size) {
 		double[] array = new double[size];
 		for (int i = 0; i < size; i++)
 			array[i] = random();
@@ -74,8 +68,7 @@ public class Randoms
 	 * @param size
 	 *            the size of random array
 	 */
-	public static double[] doubles(double min, double max, int size)
-	{
+	public static double[] doubles(double min, double max, int size) {
 		double[] array = new double[size];
 		for (int i = 0; i < size; i++)
 			array[i] = uniform(min, max);
@@ -86,32 +79,28 @@ public class Randoms
 	/**
 	 * random (uniformly distributed) double in [0, 1)
 	 */
-	public static double uniform()
-	{
+	public static double uniform() {
 		return uniform(0.0, 1.0);
 	}
 
 	/**
 	 * random (uniformly distributed) double in [min, max)
 	 */
-	public static double uniform(double min, double max)
-	{
-		return min + (max - min) * _random.nextDouble();
+	public static double uniform(double min, double max) {
+		return min + (max - min) * r.nextDouble();
 	}
 
 	/**
 	 * Return a boolean, which is true with probability p, and false otherwise.
 	 */
-	public static boolean bernoulli(double p)
-	{
+	public static boolean bernoulli(double p) {
 		return uniform() < p;
 	}
 
 	/**
 	 * Return a boolean, which is true with probability .5, and false otherwise.
 	 */
-	public static boolean bernoulli()
-	{
+	public static boolean bernoulli() {
 		return bernoulli(0.5);
 	}
 
@@ -121,16 +110,15 @@ public class Randoms
 	 * Reference: http://www.design.caltech.edu/erik/Misc/Gaussian.html
 	 * </p>
 	 * <p>
-	 * Anotehr useful reference: http://www.protonfish.com/random.shtml: G = X + X + X, where X is a random double
+	 * Anotehr useful reference: http://www.protonfish.com/random.shtml: G = X +
+	 * X + X, where X is a random double
 	 * </p>
 	 * 
 	 */
-	public static double gaussian()
-	{
+	public static double gaussian() {
 		double x, y, r;
 
-		do
-		{
+		do {
 			x = uniform(-1.0, 1.0);
 			y = uniform(-1.0, 1.0);
 			r = x * x + y * y;
@@ -142,7 +130,8 @@ public class Randoms
 	}
 
 	/**
-	 * return a real number from a Gaussian distribution with given mean and stddev
+	 * return a real number from a Gaussian distribution with given mean and
+	 * stddev
 	 * 
 	 * @param mu
 	 *            mean
@@ -150,23 +139,238 @@ public class Randoms
 	 *            stddev
 	 * 
 	 */
-	public static double gaussian(double mu, double sigma)
-	{
+	public static double gaussian(double mu, double sigma) {
 		return mu + sigma * gaussian();
+	}
+
+	/**
+	 * Randomly sample 1 point from Gamma Distribution with the given
+	 * parameters. The code is from Mahout (http://mahout.apache.org/),
+	 * available under Apache 2 license.
+	 * 
+	 * @param alpha
+	 *            alpha parameter for Gamma Distribution.
+	 * @param scale
+	 *            scale parameter for Gamma Distribution.
+	 * @return a sample point randomly drawn from the given distribution.
+	 */
+	public static double gamma(double alpha, double scale) {
+		double rate = 1 / scale;
+
+		if (alpha <= 0.0 || rate <= 0.0) {
+			throw new IllegalArgumentException();
+		}
+
+		double gds;
+		double b = 0.0;
+
+		// CASE A: Acceptance rejection algorithm gs
+		if (alpha < 1.0) {
+			b = 1.0 + 0.36788794412 * alpha; // Step 1
+			while (true) {
+				double p = b * r.nextDouble();
+				// Step 2. Case gds <= 1
+				if (p <= 1.0) {
+					gds = Math.exp(Math.log(p) / alpha);
+					if (Math.log(r.nextDouble()) <= -gds) {
+						return gds / rate;
+					}
+				}
+				// Step 3. Case gds > 1
+				else {
+					gds = -Math.log((b - p) / alpha);
+					if (Math.log(r.nextDouble()) <= ((alpha - 1.0) * Math
+							.log(gds))) {
+						return gds / rate;
+					}
+				}
+			}
+		}
+		// CASE B: Acceptance complement algorithm gd (gaussian distribution,
+		// box muller transformation)
+		else {
+			double ss = 0.0;
+			double s = 0.0;
+			double d = 0.0;
+
+			// Step 1. Preparations
+			if (alpha != -1.0) {
+				ss = alpha - 0.5;
+				s = Math.sqrt(ss);
+				d = 5.656854249 - 12.0 * s;
+			}
+
+			// Step 2. Normal deviate
+			double v12;
+			double v1;
+
+			do {
+				v1 = 2.0 * r.nextDouble() - 1.0;
+				double v2 = 2.0 * r.nextDouble() - 1.0;
+				v12 = v1 * v1 + v2 * v2;
+			} while (v12 > 1.0);
+
+			double t = v1 * Math.sqrt(-2.0 * Math.log(v12) / v12);
+			double x = s + 0.5 * t;
+			gds = x * x;
+
+			if (t >= 0.0) { // Immediate acceptance
+				return gds / rate;
+			}
+
+			double u = r.nextDouble();
+			if (d * u <= t * t * t) { // Squeeze acceptance
+				return gds / rate;
+			}
+
+			double q0 = 0.0;
+			double si = 0.0;
+			double c = 0.0;
+
+			// Step 4. Set-up for hat case
+			if (alpha != -1.0) {
+				double rr = 1.0 / alpha;
+				double q9 = 0.0001710320;
+				double q8 = -0.0004701849;
+				double q7 = 0.0006053049;
+				double q6 = 0.0003340332;
+				double q5 = -0.0003349403;
+				double q4 = 0.0015746717;
+				double q3 = 0.0079849875;
+				double q2 = 0.0208333723;
+				double q1 = 0.0416666664;
+
+				q0 = ((((((((q9 * rr + q8) * rr + q7) * rr + q6) * rr + q5)
+						* rr + q4)
+						* rr + q3)
+						* rr + q2)
+						* rr + q1)
+						* rr;
+
+				if (alpha > 3.686) {
+					if (alpha > 13.022) {
+						b = 1.77;
+						si = 0.75;
+						c = 0.1515 / s;
+					} else {
+						b = 1.654 + 0.0076 * ss;
+						si = 1.68 / s + 0.275;
+						c = 0.062 / s + 0.024;
+					}
+				} else {
+					b = 0.463 + s - 0.178 * ss;
+					si = 1.235;
+					c = 0.195 / s - 0.079 + 0.016 * s;
+				}
+			}
+
+			double v, q;
+			double a9 = 0.104089866;
+			double a8 = -0.112750886;
+			double a7 = 0.110368310;
+			double a6 = -0.124385581;
+			double a5 = 0.142873973;
+			double a4 = -0.166677482;
+			double a3 = 0.199999867;
+			double a2 = -0.249999949;
+			double a1 = 0.333333333;
+
+			// Step 5. Calculation of q
+			if (x > 0.0) {
+				// Step 6.
+				v = t / (s + s);
+				if (Math.abs(v) > 0.25) {
+					q = q0 - s * t + 0.25 * t * t + (ss + ss)
+							* Math.log(1.0 + v);
+				}
+				// Step 7. Quotient acceptance
+				else {
+					q = q0
+							+ 0.5
+							* t
+							* t
+							* ((((((((a9 * v + a8) * v + a7) * v + a6) * v + a5)
+									* v + a4)
+									* v + a3)
+									* v + a2)
+									* v + a1) * v;
+				}
+				if (Math.log(1.0 - u) <= q) {
+					return gds / rate;
+				}
+			}
+
+			double e7 = 0.000247453;
+			double e6 = 0.001353826;
+			double e5 = 0.008345522;
+			double e4 = 0.041664508;
+			double e3 = 0.166666848;
+			double e2 = 0.499999994;
+			double e1 = 1.000000000;
+
+			// Step 8. Double exponential deviate t
+			while (true) {
+				double sign_u;
+				double e;
+				do { // Step 9. Rejection of t
+					e = -Math.log(r.nextDouble());
+					u = r.nextDouble();
+					u = u + u - 1.0;
+					sign_u = (u > 0) ? 1.0 : -1.0;
+					t = b + (e * si) * sign_u;
+				} while (t <= -0.71874483771719);
+
+				// Step 10. New q(t)
+				v = t / (s + s);
+
+				if (Math.abs(v) > 0.25) {
+					q = q0 - s * t + 0.25 * t * t + (ss + ss)
+							* Math.log(1.0 + v);
+				} else {
+					q = q0
+							+ 0.5
+							* t
+							* t
+							* ((((((((a9 * v + a8) * v + a7) * v + a6) * v + a5)
+									* v + a4)
+									* v + a3)
+									* v + a2)
+									* v + a1) * v;
+				}
+
+				// Step 11.
+				if (q <= 0.0) {
+					continue;
+				}
+
+				// Step 12. Hat acceptance
+				double w;
+				if (q > 0.5) {
+					w = Math.exp(q) - 1.0;
+				} else {
+					w = ((((((e7 * q + e6) * q + e5) * q + e4) * q + e3) * q + e2)
+							* q + e1)
+							* q;
+				}
+
+				if (c * u * sign_u <= w * Math.exp(e - 0.5 * t * t)) {
+					x = s + 0.5 * t;
+					return x * x / rate;
+				}
+			}
+		}
 	}
 
 	/**
 	 * Return an integer with a Poisson distribution with mean lambda.
 	 */
-	public static int poisson(double lambda)
-	{
+	public static int poisson(double lambda) {
 		// using algorithm given by Knuth
 		// see http://en.wikipedia.org/wiki/Poisson_distribution
 		int k = 0;
 		double p = 1.0;
 		double L = Math.exp(-lambda);
-		do
-		{
+		do {
 			k++;
 			p *= uniform();
 		} while (p >= L);
@@ -176,45 +380,46 @@ public class Randoms
 	/**
 	 * Return a real number with a Pareto distribution with parameter alpha.
 	 */
-	public static double pareto(double alpha)
-	{
+	public static double pareto(double alpha) {
 		return Math.pow(1 - uniform(), -1.0 / alpha) - 1.0;
 	}
 
 	/**
 	 * Return a real number with a Cauchy distribution.
 	 */
-	public static double cauchy()
-	{
+	public static double cauchy() {
 		return Math.tan(Math.PI * (uniform() - 0.5));
 	}
 
 	/**
-	 * Return a number from a discrete distribution: i with probability a[i]. Precondition: array entries are
-	 * nonnegative and their sum (very nearly) equals 1.0.
+	 * Return a number from a discrete distribution: i with probability a[i].
+	 * Precondition: array entries are nonnegative and their sum (very nearly)
+	 * equals 1.0.
 	 */
-	public static int discrete(double[] a)
-	{
+	public static int discrete(double[] a) {
 		double EPSILON = 1E-14;
 		double sum = 0.0;
-		for (int i = 0; i < a.length; i++)
-		{
-			if (a[i] < 0.0) throw new IllegalArgumentException("array entry " + i + " is negative: " + a[i]);
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] < 0.0)
+				throw new IllegalArgumentException("array entry " + i
+						+ " is negative: " + a[i]);
 			sum = sum + a[i];
 		}
-		if (sum > 1.0 + EPSILON || sum < 1.0 - EPSILON) throw new IllegalArgumentException(
-				"sum of array entries not equal to one: " + sum);
+		if (sum > 1.0 + EPSILON || sum < 1.0 - EPSILON)
+			throw new IllegalArgumentException(
+					"sum of array entries not equal to one: " + sum);
 
-		// the for loop may not return a value when both r is (nearly) 1.0 and when the
-		// cumulative sum is less than 1.0 (as a result of floating-point roundoff error)
-		while (true)
-		{
+		// the for loop may not return a value when both r is (nearly) 1.0 and
+		// when the
+		// cumulative sum is less than 1.0 (as a result of floating-point
+		// roundoff error)
+		while (true) {
 			double r = uniform();
 			sum = 0.0;
-			for (int i = 0; i < a.length; i++)
-			{
+			for (int i = 0; i < a.length; i++) {
 				sum = sum + a[i];
-				if (sum > r) return i;
+				if (sum > r)
+					return i;
 			}
 		}
 	}
@@ -222,8 +427,7 @@ public class Randoms
 	/**
 	 * Return a real number from an exponential distribution with rate lambda.
 	 */
-	public static double exp(double lambda)
-	{
+	public static double exp(double lambda) {
 		return -Math.log(1 - uniform()) / lambda;
 	}
 
@@ -236,8 +440,7 @@ public class Randoms
 	 *            the exception values when generating integers, sorted first
 	 * @return next no-repeated random integer
 	 */
-	public static int nextNoRepeatInt(int range, int... exceptions)
-	{
+	public static int nextNoRepeatInt(int range, int... exceptions) {
 		return nextNoRepeatInt(0, range, exceptions);
 	}
 
@@ -253,19 +456,17 @@ public class Randoms
 	 * @return next no-repeated random integer
 	 */
 
-	public static int nextNoRepeatInt(int min, int max, int... exceptions)
-	{
+	public static int nextNoRepeatInt(int min, int max, int... exceptions) {
 		int next;
-		while (true)
-		{
-			next = min + _random.nextInt(max - min);
-			if (exceptions != null && exceptions.length > 0 && Arrays.binarySearch(exceptions, next) >= 0)
-			{
+		while (true) {
+			next = min + r.nextInt(max - min);
+			if (exceptions != null && exceptions.length > 0
+					&& Arrays.binarySearch(exceptions, next) >= 0) {
 				continue;
 			}
-			if (_tempList.contains(next)) continue;
-			else
-			{
+			if (_tempList.contains(next))
+				continue;
+			else {
 				_tempList.add(next);
 				break;
 			}
@@ -277,19 +478,15 @@ public class Randoms
 	 * Generate no repeat {@code size} indexes from {@code min} to {@code max}
 	 * 
 	 */
-	public static int[] indexs(int size, int min, int max)
-	{
+	public static int[] indexs(int size, int min, int max) {
 		Set<Integer> used = new HashSet<>();
 		int[] index = new int[size];
 
-		for (int i = 0; i < index.length; i++)
-		{
-			while (true)
-			{
+		for (int i = 0; i < index.length; i++) {
+			while (true) {
 				int ind = uniform(min, max);
 
-				if (!used.contains(ind))
-				{
+				if (!used.contains(ind)) {
 					index[i] = ind;
 					used.add(ind);
 					break;
@@ -300,8 +497,7 @@ public class Randoms
 		return index;
 	}
 
-	public static void clearCache()
-	{
+	public static void clearCache() {
 		_tempList.clear();
 	}
 
@@ -318,33 +514,32 @@ public class Randoms
 	 * @throws Exception
 	 *             if the range is less than length, an exception will be thrown
 	 */
-	public static int[] nextNoRepeatIntArray(int length, int range) throws Exception
-	{
+	public static int[] nextNoRepeatIntArray(int length, int range)
+			throws Exception {
 		return nextNoRepeatIntArray(length, 0, range, null);
 	}
 
-	public static int[] nextNoRepeatIntArray(int length, int range, int... exceptions) throws Exception
-	{
+	public static int[] nextNoRepeatIntArray(int length, int range,
+			int... exceptions) throws Exception {
 		return nextNoRepeatIntArray(length, 0, range, exceptions);
 	}
 
-	public static int[] nextNoRepeatIntArray(int length, int min, int max) throws Exception
-	{
+	public static int[] nextNoRepeatIntArray(int length, int min, int max)
+			throws Exception {
 		return nextNoRepeatIntArray(length, min, max, null);
 	}
 
-	public static int[] nextNoRepeatIntArray(int length, int min, int max, int... exceptions) throws Exception
-	{
+	public static int[] nextNoRepeatIntArray(int length, int min, int max,
+			int... exceptions) throws Exception {
 		int maxLen = max - min; // because max itself is not counted
-		if (maxLen < length) throw new Exception("The range is less than legth");
+		if (maxLen < length)
+			throw new Exception("The range is less than legth");
 
 		int[] index = new int[length];
-		if (maxLen == length)
-		{
+		if (maxLen == length) {
 			for (int i = 0; i < length; i++)
 				index[i] = min + i;
-		} else
-		{
+		} else {
 			Randoms.clearCache();
 			for (int i = 0; i < index.length; i++)
 				index[i] = Randoms.nextNoRepeatInt(min, max, exceptions);
@@ -353,8 +548,7 @@ public class Randoms
 		return index;
 	}
 
-	public static int[] ints(int range, int size)
-	{
+	public static int[] ints(int range, int size) {
 		int[] data = new int[size];
 
 		for (int i = 0; i < size; i++)
@@ -363,8 +557,7 @@ public class Randoms
 		return data;
 	}
 
-	public static int[] ints(int min, int max, int size)
-	{
+	public static int[] ints(int min, int max, int size) {
 		int[] data = new int[size];
 
 		for (int i = 0; i < size; i++)
@@ -373,23 +566,22 @@ public class Randoms
 		return data;
 	}
 
-	public static List<Double> list(int size)
-	{
+	public static List<Double> list(int size) {
 		return list(size, 0, 1, false);
 	}
 
-	public static List<Double> list(int size, int min, int max)
-	{
+	public static List<Double> list(int size, int min, int max) {
 		return list(size, min, max, false);
 	}
 
-	public static List<Double> list(int size, int min, int max, boolean isInteger)
-	{
+	public static List<Double> list(int size, int min, int max,
+			boolean isInteger) {
 		List<Double> list = new ArrayList<>(size);
-		for (int i = 0; i < size; i++)
-		{
-			if (isInteger) list.add(uniform(min, max) + 0.0);
-			else list.add(uniform(min + 0.0, max + 0.0));
+		for (int i = 0; i < size; i++) {
+			if (isInteger)
+				list.add(uniform(min, max) + 0.0);
+			else
+				list.add(uniform(min + 0.0, max + 0.0));
 		}
 
 		return list;
@@ -404,19 +596,15 @@ public class Randoms
 	 *            the maximum value
 	 * @return a permutation
 	 */
-	public static List<Integer> permute(int min, int max)
-	{
+	public static List<Integer> permute(int min, int max) {
 		List<Integer> list = new ArrayList<>();
 
 		int len = max - min + 1;
-		for (int i = 0; i < len; i++)
-		{
-			while (true)
-			{
+		for (int i = 0; i < len; i++) {
+			while (true) {
 				int index = uniform(min, max + 1);
 
-				if (!list.contains(index))
-				{
+				if (!list.contains(index)) {
 					list.add(index);
 					break;
 				}
@@ -427,20 +615,16 @@ public class Randoms
 	}
 
 	@Test
-	public void example_permute()
-	{
-		for (int i = 0; i < 20; i++)
-		{
+	public void example_permute() {
+		for (int i = 0; i < 20; i++) {
 			List<Integer> perm = Randoms.permute(1, 5);
 			Logs.debug("Iteration " + (i + 1) + " : " + perm);
 		}
 	}
 
 	@Test
-	public void example_gaussian()
-	{
-		for (int i = 0; i < 20; i++)
-		{
+	public void example_gaussian() {
+		for (int i = 0; i < 20; i++) {
 			Logs.debug(gaussian());
 		}
 	}

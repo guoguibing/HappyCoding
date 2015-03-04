339f84d0263d3c1d5147d6117cf8dbad3ed5f65d
guoguibing <gbguo@smu.edu.sg>
2015-03-04 07:47:55 -0300

    Gaussian distribution updated

--- happycoding/src/main/java/happy/coding/math/Gaussian.java
+++ happycoding/src/main/java/happy/coding/math/Gaussian.java
@@ -1,86 +1,118 @@
 package happy.coding.math;
 
-import happy.coding.io.Logs;
-
-import org.junit.Test;
-
 /**
  * <h3>Gaussian Distribution</h3>
  * <p>
  * The approximation is accurate to absolute error less than $8 * 10^(-16)$. <br/>
  * Reference paper: George Marsaglia, Evaluating the Normal Distribution.<br/>
- * Reference page: http://introcs.cs.princeton.edu/java/21function/
+ * 
+ * http://introcs.cs.princeton.edu/java/21function/ <br>
+ * http://www.jstatsoft.org/v11/a04/paper
  * </p>
  * 
- * @author Guo Guibing
- * 
  */
-public class Gaussian
-{
+public class Gaussian {
 
 	/**
-	 * Standard Gaussian distribution
-	 * 
-	 * @return phi(x) = standard Gaussian pdf
+	 * standard Gaussian pdf
 	 */
-	public static double pdf(double x)
-	{
-		return Math.exp(-0.5 * x * x) / Math.sqrt(2 * Math.PI);
+	public static double pdf(double x) {
+		return Math.exp(-x * x / 2) / Math.sqrt(2 * Math.PI);
 	}
 
 	/**
-	 * Gaussian distribution
-	 * 
-	 * @param mu
-	 *            mean value
-	 * @param sigma
-	 *            standard deviation
-	 * @return phi(x) = Gaussian pdf
+	 * Gaussian pdf with mean mu and stddev sigma
 	 */
-	public static double pdf(double x, double mu, double sigma)
-	{
+	public static double pdf(double x, double mu, double sigma) {
 		return pdf((x - mu) / sigma) / sigma;
 	}
 
 	/**
-	 * Standard Gaussian cdf using Taylor approximation
+	 * standard Gaussian cdf using Taylor approximation;
 	 * 
-	 * @return $\Phi(z) = \int_{-\infinity}^z pdf(x) dx$
+	 * @return the probability that a random variable distributed according to the standard normal distribution (mean =
+	 *         0 and stdev = 1) produces a value less than z
 	 */
-	public static double cdf(double z)
-	{
-		if (z < -8.0) return 0.0;
-		if (z > 8.0) return 1.0;
-
+	public static double cdf(double z) {
+		if (z < -8.0)
+			return 0.0;
+		if (z > 8.0)
+			return 1.0;
 		double sum = 0.0, term = z;
-		for (int i = 3; sum + term != sum; i += 2)
-		{
-			sum += term;
+		for (int i = 3; sum + term != sum; i += 2) {
+			sum = sum + term;
 			term = term * z * z / i;
 		}
-
-		return 0.5 + pdf(z) * sum;
+		return 0.5 + sum * pdf(z);
 	}
 
 	/**
-	 * Gaussian cdf using Taylor approximation
+	 * Gaussian cdf with mean mu and stddev sigma
+	 * 
+	 * @returnt he probability that a random variable X distributed normally with mean mu and stdev sigma produces a
+	 *          value less than z
 	 */
-	public static double cdf(double z, double mu, double sigma)
-	{
+	public static double cdf(double z, double mu, double sigma) {
 		return cdf((z - mu) / sigma);
 	}
 
-	@Test
-	public void examples()
-	{
-		double xs[] = { 0.1, 1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, -1.1, -3.3, -5.5, -7.7, 1.96 };
-
-		for (int i = 0; i < xs.length; i++)
-		{
-			double x = xs[i];
-			double cdf = cdf(x);
-
-			Logs.debug("x = " + x + ", cdf = " + cdf);
-		}
+	/**
+	 * Compute z for standard normal such that cdf(z) = y via bisection search
+	 */
+	public static double PhiInverse(double y) {
+		return PhiInverse(y, .00000001, -8, 8);
 	}
+
+	private static double PhiInverse(double y, double delta, double lo, double hi) {
+		double mid = lo + (hi - lo) / 2;
+		if (hi - lo < delta)
+			return mid;
+		if (cdf(mid) > y)
+			return PhiInverse(y, delta, lo, mid);
+		else
+			return PhiInverse(y, delta, mid, hi);
+	}
+
+	/**
+	 * Compute z for standard normal such that cdf(z, mu, sigma) = y via bisection search
+	 */
+	public static double PhiInverse(double y, double mu, double sigma) {
+		return PhiInverse2(y, mu, sigma, .00000001, (mu - 8 * sigma), (mu + 8 * sigma));
+	}
+
+	private static double PhiInverse2(double y, double mu, double sigma, double delta, double lo, double hi) {
+		double mid = lo + (hi - lo) / 2;
+		if (hi - lo < delta)
+			return mid;
+		if (cdf(mid, mu, sigma) > y)
+			return PhiInverse2(y, mu, sigma, delta, lo, mid);
+		else
+			return PhiInverse2(y, mu, sigma, delta, mid, hi);
+	}
+
+	public static void main(String[] args) {
+
+		// This prints out the values of the probability density function for N(2.0.0.6)
+		// A graph of this is here: http://www.cs.bu.edu/fac/snyder/cs237/Lecture%20Materials/GaussianExampleJava.png
+		double mu = 2.0;
+		double sigma = 1.5;
+		System.out.println("PDF for N(2.0,0.6) in range [-4..8]:");
+		for (double z = -4.0; z <= 8.0; z += 0.2)
+			System.out.format("%.1f\t%.4f\n", z, pdf(z, mu, sigma));
+
+		// This prints out the values of the cumulative density function for N(2.0.0.6)
+		// A graph of this is here: http://www.cs.bu.edu/fac/snyder/cs237/Lecture%20Materials/GaussianExample2Java.png
+		System.out.println("CDF for N(2.0,0.6) in range [-4..8]:");
+		for (double z = -4.0; z <= 8.0; z += 0.2)
+			System.out.format("%.1f\t%.4f\n", z, cdf(z, mu, sigma));
+
+		// Calculates the probability that for N(2.0, 0.6), the random variable produces a value less than 3.45
+		System.out.format("\nIf X ~ N(2.0, 1.5), then P(X <= 3.2) is %.4f\n", cdf(3.2, 2.0, 1.5));
+
+		// Calculates the value x for X ~ N(2.0, 0.6) which is the 78.81% cutoff (i.e., 78.81% of the values lie below x and 21.19% above). 
+		System.out.format("\nIf X ~ N(2.0, 1.5), then x such that P(X <= x ) = 0.7881 is %.4f\n",
+				PhiInverse(0.7881, 2.0, 1.5));
+
+	}
+
 }
